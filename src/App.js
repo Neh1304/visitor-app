4e6c127c8c506718fe260b841a2f0e92f75ae1df
Neh1304 <itsnehasiju@gmail.com>
2024-12-15 17:57:34 +0530

    success

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Add from './components/Add';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewAllVisitors from './components/ViewAllVisitors';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Add/>} />
+      <Route path='/viewall' element={<ViewAllVisitors/>} />
+    </Routes>
+     </BrowserRouter>
+  );
+}
+export default App;
